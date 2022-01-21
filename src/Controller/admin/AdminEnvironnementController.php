f9e33941b976b64e437bdc42ea165e4f3440f09a
laure <laure@DESKTOP-52V32IN.home>
2022-01-21 18:51:52 +0100

    Phase 16 : ORM (gestion des environnements)

---
+++ src/Controller/admin/AdminEnvironnementController.php
@@ -0,0 +1,68 @@
+<?php
+
+
+namespace App\Controller\admin;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use App\Entity\Environnement; 
+use App\Repository\EnvironnementRepository;
+use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\ORM\EntityManagerInterface; 
+
+
+class AdminEnvironnementController extends AbstractController {
+    /**
+     * var EnvironnementRepository
+     */
+    private $repository ;
+    /**
+     * 
+     * @var EntityManagerInterface
+     */
+    private $om ;
+    /**
+     * 
+     * @param EnvironnementRepository $repository
+     * @param EntityManagerInterface $om
+     */
+    function __construct(EnvironnementRepository $repository, EntityManagerInterface $om) {
+        $this->repository = $repository;
+        $this->om = $om;
+    }
+    /**
+     * @Route("/admin/environnement/suppr/{id}", name="admin.environnement.suppr")
+     * @param Environnement $environnement
+     * @return Response
+     */
+    public function suppr(Environnement $environnement): Response{
+        $this->om->remove($environnement);
+        $this->om->flush();
+        return $this->redirectToRoute('admin.environnements');
+    } 
+    /**
+     * @Route("/admin/environnement/ajout", name="admin.environnement.ajout")
+     * @param Request $request
+     * @return Response
+     */
+    public function ajout(Request $request) : Response {
+        $nomEnvironnement =$request->get('nom');
+        $environnement= new Environnement();
+        $environnement->setNom($nomEnvironnement);
+        $this->om->persist($environnement);
+        $this->om->flush();
+        return $this->redirectToRoute('admin.environnements');
+        
+    }
+   /**
+     * @Route("/admin/environnements", name="admin.environnements")
+     * @return Response
+     */
+    public function index(): Response{
+        $environnements = $this->repository->findAll();
+        return $this->render("admin/admin.environnements.html.twig", [
+            'environnements' =>$environnements
+        ]);
+    }
+}
