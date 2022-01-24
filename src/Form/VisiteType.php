6db55fb7984929f32b498cc500665ec284393351
laure <laure@DESKTOP-52V32IN.home>
2022-01-24 16:54:57 +0100

    Phase 19 : gestion des images

--- src/Form/VisiteType.php
+++ src/Form/VisiteType.php
@@ -2,13 +2,15 @@
 
 namespace App\Form;
 
+use App\Entity\Environnement;
 use App\Entity\Visite;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use App\Entity\Environnement;
-use Symfony\Bridge\Doctrine\Form\Type\EntityType ;
+
 
 class VisiteType extends AbstractType
 {
@@ -28,6 +30,9 @@ class VisiteType extends AbstractType
                 'multiple' => true,
                 'required' => false
             ])
+            ->add('imageFile', FileType::class, ['required' => false,
+                'label' => 'image'
+                ])
             ->add('submit', SubmitType::class)
         ;
     }
